75f19079f58b272f24e4864f3ef9dd5a5f478b15
tristan-caetano <tristancostacaetano@gmail.com>
2022-12-06 21:14:35 -0500

    Adding running server functionality, begin linux testing

--- jar_collector.py
+++ jar_collector.py
@@ -5,6 +5,37 @@
 # Importing packages
 from os.path import exists
 import pandas as pd
+import glob
+import os
+
+# Script for running server
+def run_server():
+
+    # Naming csv file
+    server_list = "server_list.csv"
+
+    list_num = 1
+
+    # Checking if there is already a csv file
+    if exists(server_list):
+        list = pd.read_csv(server_list)
+    else:
+        print("No .csv found.\nMake sure the .csv is named 'server_list.csv'.\nExiting to main menu.\n")
+        main_menu()
+    
+    print("\nPlease input the number of the server that you would like to run.\n")
+
+    # Printing out the list of just the server names
+    for x in range(0, len(list)):
+        print(str(list_num) + ").", str(list.iloc[x][4])), "\n"
+        list_num += 1
+
+    shell_in = int(input())
+
+    shell = list['launch_shell'][shell_in - 1]
+
+    os.system("./'" + shell + "'")
+
 
 # Saving the server info to a csv file
 def save_server_info(data_list):
@@ -16,13 +47,37 @@ def save_server_info(data_list):
     if exists(server_list):
         list = pd.read_csv(server_list)
     else:
-        list = pd.DataFrame(columns=["servername", "mod", "version", "directory"])
+        list = pd.DataFrame(columns=["servername", "mod", "version", "directory", "launch_shell"])
 
     # Adding new data to list
     list.loc[len(list.index)] = data_list
 
     # Saving data to csv file
     list.to_csv(server_list, index=False)
+
+# Using the given directory, gather shell files
+def find_shells(data_list):
+
+    # Appending .sh to the directory
+    path_search = data_list[3] + "\*.sh"
+
+    # Getting list of all shell scripts in directory
+    list_of_shells = glob.glob(path_search)
+
+    # Iterator for list
+    list_num = 1
+
+    # Printing out all found files to user
+    for item in list_of_shells:
+        print(str(list_num) + ").", item), "\n"
+        list_num += 1
+    
+    # Asking user to choose file found
+    chosen_num = int(input("Which shell is the launch shell?"))
+    data_list.append(list_of_shells[chosen_num - 1])
+
+    # Saving data to csv
+    save_server_info(data_list)
 
 # Getting basic server information and directory location
 def get_server_location():
@@ -43,7 +98,7 @@ def get_server_location():
             version = input("\nWhat version of Minecraft is it?\n")
 
             # Getting the directory of the server, and appending server.properties
-            directory = input("\nInput the directory of the server folder.\n Do NOT add a '/' to the end.\n")
+            directory = input("\nInput the directory of the server folder.\nDo NOT add a '/' to the end.\n")
             server_properties = directory + "/server.properties"
 
             # Details completed, skipping if
@@ -63,8 +118,38 @@ def get_server_location():
         # Creating list containing server info
         data_list = [name, modded, version, directory]
         
-        # Sending info to be saved to a csv file
-        save_server_info(data_list)
+        # Sending info to find shell files
+        find_shells(data_list)
 
+# Main menu
+def main_menu():
 
-get_server_location()
+    # List of menu options
+    list_of_options = ["Add new server config.", 
+                        "Run server.",]
+    # Iterator for list
+    list_num = 1
+
+    while(True):
+
+        print("\nMinecraft Server Organizer\nMain Menu\n")
+
+        # Printing out all found files to user
+        for option in list_of_options:
+            print(str(list_num) + ").", option), "\n"
+            list_num += 1
+        
+        menu_in = input("\nEnter 'q' to quit.\n")
+
+        if menu_in == "q":
+            quit()
+        elif menu_in == '1':
+            get_server_location()
+        elif menu_in == '2':
+            run_server()
+        else:
+            print("\nCommand not found, please input an available number or 'q'.\n")
+
+        list_num = 1
+
+main_menu()
