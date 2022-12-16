b32e38741a8be20051f7df3ee5ffa2c28ce4a804
tristan-caetano <tristancostacaetano@gmail.com>
2022-12-16 18:37:37 -0500

    Tested with arduino and relay, NOT FOR FINAL

---
+++ remote_power/remote_power.ino
@@ -0,0 +1,18 @@
+// Tristan Caetano
+// Remote Power to Server
+
+// Server Power Pin
+#define PWR_PIN 8
+
+void setup() {
+
+  pinMode(PWR_PIN, OUTPUT);
+
+  digitalWrite(PWR_PIN, HIGH);
+  delay(100.);
+  digitalWrite(PWR_PIN, LOW);
+}
+
+void loop(){
+  
+}
